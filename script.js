afe0d26b67240ff63e46d644bbad02f12234a70a
Brandon Green <Brandon.Green@knights.ucf.edu>
2020-09-23 19:30:11 -0400

    added icons to 5 day forcast and more CSS

--- script.js
+++ script.js
@@ -66,6 +66,7 @@ $(document).ready(function () {
             url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=e4a0807b709fd21166a9113bc8472380&units=imperial"
 
         }).then(function (data) {
+
             $("#currentWeather").empty();
             var cityName = $("<h3>")
             var cityIcon = $("<img>")
@@ -99,6 +100,8 @@ $(document).ready(function () {
             humidity.text("Humidity: " + data.main.humidity + "%");
             $("#currentWeather").append(humidity);
 
+
+
             console.log(data);
             $.ajax({
 
@@ -112,6 +115,8 @@ $(document).ready(function () {
                 $("#currentWeather").append(uv)
                 var date = response.date_iso.split("T")
                 $(cityName).append(" " + date[0])
+
+
     
             })
         })
@@ -124,6 +129,7 @@ $(document).ready(function () {
             method: "GET",
             url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=e4a0807b709fd21166a9113bc8472380&units=imperial"
         }).then(function (response) {
+
             console.log(response);
             var day1 = $("<p>");
             var day2 = $("<p>");
@@ -135,7 +141,52 @@ $(document).ready(function () {
             var hum3 = $("<p>");
             var hum4 = $("<p>");
             var hum5 = $("<p>");
+            var iconimg1 = $("<img>")
+            var iconimg2 = $("<img>")
+            var iconimg3 = $("<img>")
+            var iconimg4 = $("<img>")
+            var iconimg5 = $("<img>")
 
+            var iconCode1 = response.list[0].weather[0].icon
+            var iconURL1 = "http://openweathermap.org/img/w/" + iconCode1 + ".png"
+            var iconCode2 = response.list[9].weather[0].icon
+            var iconURL2 = "http://openweathermap.org/img/w/" + iconCode2 + ".png"
+            var iconCode3 = response.list[18].weather[0].icon
+            var iconURL3 = "http://openweathermap.org/img/w/" + iconCode3 + ".png"
+            var iconCode4 = response.list[27].weather[0].icon
+            var iconURL4 = "http://openweathermap.org/img/w/" + iconCode4 + ".png"
+            var iconCode5 = response.list[36].weather[0].icon
+            var iconURL5 = "http://openweathermap.org/img/w/" + iconCode5 + ".png"
+
+            iconimg1.attr('src', iconURL1);
+            iconimg1.attr('width', '75px')
+            iconimg1.attr('height', '75px')
+            iconimg1.attr('class', 'cityIcon')
+            $("#dayOne").append(iconimg1)
+
+            iconimg2.attr('src', iconURL2);
+            iconimg2.attr('width', '75px')
+            iconimg2.attr('height', '75px')
+            iconimg2.attr('class', 'cityIcon')
+            $("#dayTwo").append(iconimg2)
+
+            iconimg3.attr('src', iconURL3);
+            iconimg3.attr('width', '75px')
+            iconimg3.attr('height', '75px')
+            iconimg3.attr('class', 'cityIcon')
+            $("#dayThree").append(iconimg3)
+
+            iconimg4.attr('src', iconURL4);
+            iconimg4.attr('width', '75px')
+            iconimg4.attr('height', '75px')
+            iconimg4.attr('class', 'cityIcon')
+            $("#dayFour").append(iconimg4)
+
+            iconimg5.attr('src', iconURL5);
+            iconimg5.attr('width', '75px')
+            iconimg5.attr('height', '75px')
+            iconimg5.attr('class', 'cityIcon')
+            $("#dayFive").append(iconimg5)
 
             day1.text("Temperature: " + response.list[0].main.temp + " °F");
             $("#dayOne").append(day1);
@@ -158,6 +209,9 @@ $(document).ready(function () {
             $("#dayFour").append(hum4)
             hum5.text("Humidity: " + response.list[36].main.humidity + "%")
             $("#dayFive").append(hum5)
+
+
+
 
         })
 
